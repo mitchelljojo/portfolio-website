6ccb3fc67e3bd5738edab0aa9059ff9939e5d650
MichiJojo94 <108657292+mitchelljojo@users.noreply.github.com>
2026-06-04 23:58:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     hmr: process.env.DISABLE_HMR !== "true",
+//     watch: process.env.DISABLE_HMR === "true" ? null : {},
+//   },
+// });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: process.env.DISABLE_HMR !== "true",
-    watch: process.env.DISABLE_HMR === "true" ? null : {},
-  },
-});
+  base: '/portfolio-website/',
+})
